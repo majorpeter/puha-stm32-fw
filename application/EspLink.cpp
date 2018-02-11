42ce1f1348c776f0c142e218ad2ea32c748e3271
Major Péter <majorpeter29@gmail.com>
2018-02-11 22:47:31 +0100

    changed EspLink GPIO's to inputs with pull-up

--- application/EspLink.cpp
+++ application/EspLink.cpp
@@ -9,16 +9,10 @@
 
 EspLink::EspLink(GPIO_TypeDef * const resetGpioPort, uint16_t resetGpioPin,
         GPIO_TypeDef * const chPdGpioPort, uint16_t chPdGpioPin): UsartSerialInterface(115200) {
-    GPIO_SetBits(chPdGpioPort, chPdGpioPin);
-
     GPIO_InitTypeDef GPIO_InitStruct;
-    GPIO_InitStruct.GPIO_Mode = GPIO_Mode_Out_PP;
+    GPIO_InitStruct.GPIO_Mode = GPIO_Mode_IPU;
     GPIO_InitStruct.GPIO_Pin = chPdGpioPin;
     GPIO_InitStruct.GPIO_Speed = GPIO_Speed_10MHz;
     GPIO_Init(chPdGpioPort, &GPIO_InitStruct);
-
-    GPIO_SetBits(resetGpioPort, resetGpioPin);
-
-    GPIO_InitStruct.GPIO_Pin = resetGpioPin;
     GPIO_Init(resetGpioPort, &GPIO_InitStruct);
 }
