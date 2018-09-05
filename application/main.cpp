09f34d9a776fda44f600edf8f16bf49907e23bcf
Major Péter <majorpeter29@gmail.com>
2018-09-05 08:29:13 +0200

    added hardware_config.h for conditional H/W driver creation

--- application/main.cpp
+++ application/main.cpp
@@ -16,6 +16,7 @@
 #include "LightSensorNode.h"
 #include "MotionSensor.h"
 #include "MotionSensorNode.h"
+#include "hardware_config.h"
 
 #include <string.h>
 
@@ -27,11 +28,14 @@ int main() {
 
     RootNode::getInstance()->addChild(new SystemNode());
 
+#ifdef HWCONFIG_LEDSTRIP_PRESENT
     LedStripController ledStrip(Hardware::LedStripDataOutPort, Hardware::LedStripDataOutPin, 180, 0, false);
     ledStrip.init();
     ledStrip.writeLeds(NULL, 0);
     RootNode::getInstance()->addChild(new LedStripControllerNode(&ledStrip));
+#endif /* HWCONFIG_LEDSTRIP_PRESENT */
 
+#ifdef HWCONFIG_HTU_PRESENT
     Stm32F1I2c::InitStruct initStruct = {
             .i2cPeriph = Hardware::Htu21DI2c,
             .sdaSclPort = Hardware::Htu21DSdaSclPort,
@@ -42,25 +46,43 @@ int main() {
     Stm32F1I2c* htu21dI2c = new Stm32F1I2c(&initStruct);
     Htu21D* htu21d = new Htu21D(htu21dI2c);
     RootNode::getInstance()->addChild(new Htu21DNode(htu21d));
+#endif /* HWCONFIG_HTU_PRESENT */
 
+#ifdef HWCONFIG_LIGHTSENSOR_PRESENT
     LightSensor* lightSensor = new LightSensor(4096);
     LightSensorNode* lightSensorNode = new LightSensorNode(lightSensor);
     RootNode::getInstance()->addChild(lightSensorNode);
+#endif /* HWCONFIG_LIGHTSENSOR_PRESENT */
 
+#ifdef HWCONFIG_MOTIONSENSOR_PRESENT
     MotionSensor* motionSensor = new MotionSensor();
     RootNode::getInstance()->addChild(new MotionSensorNode(motionSensor));
+#endif /* HWCONFIG_MOTIONSENSOR_PRESENT */
 
+#ifdef HWCONFIG_USART_PRESENT
     UsartSerialInterface* serialInterface = new UsartSerialInterface(460800);
     ProtocolParser* protocol = new ProtocolParser(serialInterface);
     serialInterface->listen();
+#endif /* HWCONFIG_USART_PRESENT */
 
     Os::start();
     while (1) {
         Hardware::LiveLedToggle();
+#ifdef HWCONFIG_USART_PRESENT
         protocol->handler();
+#endif
+
+#ifdef HWCONFIG_HTU_PRESENT
         htu21d->handler();
+#endif
+
+#ifdef HWCONFIG_LIGHTSENSOR_PRESENT
         lightSensor->handler();
+#endif
+
+#ifdef HWCONFIG_MOTIONSENSOR_PRESENT
         motionSensor->handler();
+#endif
     }
 
     return 0;
